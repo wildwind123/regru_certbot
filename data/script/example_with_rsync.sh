6860184cda557a924adcd8072fe88292e55d5d8f
ganbatte <vinokurovvin@list.ru>
2025-10-01 18:54:26 +0900

    remote nginx reloaded

--- data/script/example_with_rsync.sh
+++ data/script/example_with_rsync.sh
@@ -8,4 +8,5 @@ rsync -azLe "ssh -i /home/host/.ssh/id_rsa" \
  /home/host/projects/github/regru/data/etc/letsencrypt/live/host.ru/fullchain.pem \
  /home/host/projects/github/regru/data/etc/letsencrypt/live/host.ru/privkey.pem \
  root@host.ru:/root/projects/github/cert/_.host.ru/ | grep -q . && \
- ssh -i /home/host/.ssh/id_rsa root@host.ru "sudo /usr/sbin/nginx -s reload"
+ ssh -i /home/host/.ssh/id_rsa root@host.ru "sudo /usr/sbin/nginx -s reload" && \
+ echo "remote nginx reloaded"
