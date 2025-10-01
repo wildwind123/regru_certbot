350b75f9c9e4c637a3c3f609797ca3b82f591c79
ganbatte <vinokurovvin@list.ru>
2025-10-01 17:01:00 +0900

    renew.sh

---
+++ renew.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+OUTPUT=$(docker compose -f /home/ayar/projects/github/regru_certbot/docker-compose-renew.yaml up)
+
+echo "$OUTPUT"
+
+if echo "$OUTPUT" | grep -q -- "---success---"; then
+    echo "✅ Found success marker, reloading nginx..."
+    nginx -s reload
+else
+    echo "ℹ️ No success marker, skipping nginx reload."
+fi
